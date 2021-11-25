3c19e76b8be224a5e2acdfa8813e013a974e70cb
Ubuntu <ubuntu@ip-172-31-17-200.ec2.internal>
2021-11-25 02:56:24 +0000

    hello World Style.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright © 2021 Dylan Melo All rights reserved
+//
+// Created by Dylan Melo
+// Created on November 2021
+// This is a “Hello world” with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
